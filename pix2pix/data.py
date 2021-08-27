fe1c9e1c7ed5c1907a4fbe1963468fbbaebc61d4
Colin Chaigneau <chaigneaucolin@gmail.com>
2021-08-27 16:07:25 +0200

    add verbose to dataset loading

--- pix2pix/data.py
+++ pix2pix/data.py
@@ -1,4 +1,4 @@
-import os 
+import os
 import glob
 import imageio
 import numpy as np
@@ -15,7 +15,7 @@ def load_data(host='drive', dataset='facades'):
     Returns:
         data_train, data_val, data_test (tf.Tensor, dtype='float32'): The three datasets (train, val, test) as tensors with 'float32' type
     """
-    
+
     if host == 'drive':
         directory = '/content/drive/MyDrive/pix2pix/datasets'
         if dataset == 'facades':
@@ -25,23 +25,49 @@ def load_data(host='drive', dataset='facades'):
         if dataset == 'facades':
             directory += '/facades'
     #TODO create directories for other datasets and check validity of host and dataset
-    
+
     data_train = []
     data_val = []
     data_test = []
-    for im_path in glob.glob(directory+"/train/*.jpg"):
+
+    train_pathes = glob.glob(directory+"/train/*.jpg")
+    val_pathes   = glob.glob(directory+"/val/*.jpg")
+    test_pathes  = glob.glob(directory+"/test/*.jpg")
+
+
+    # Process train pathes
+    ite_count, ite_total = 0, len(train_pathes)  # Track progress in imports
+    for im_path in train_pathes:
+        if ite_count % (ite_total // 10) == 0:
+            print(f'   Import train in progress... {int(ite_count * 100 / ite_total):02d}%', end='\r')
         data_train.append(imageio.core.asarray(imageio.imread(im_path)))
+        ite_count += 1
+    print('Import train  -------------- DONE!')
 
-    for im_path in glob.glob(directory+"/val/*.jpg"):
+
+    # Process val pathes
+    ite_count, ite_total = 0, len(val_pathes)
+    for im_path in val_pathes:
+        if ite_count % (ite_total // 10) == 0:
+            print(f'   Import val in progress... {int(ite_count * 100 / ite_total):02d}%', end='\r')
         data_val.append(imageio.core.asarray(imageio.imread(im_path)))
+        ite_count += 1
+    print('Import val    -------------- DONE!')
 
-    for im_path in glob.glob(directory+"/test/*.jpg"):
+
+    # Process test pathes
+    ite_count, ite_total = 0, len(test_pathes)
+    for im_path in test_pathes:
+        if ite_count % (ite_total // 10) == 0:
+            print(f'   Import test progress... {int(ite_count * 100 / ite_total):02d}%', end='\r')
         data_test.append(imageio.core.asarray(imageio.imread(im_path)))
+        ite_count += 1
+    print('Import test   -------------- DONE!')
 
     data_train = convert_to_tensor(np.array(data_train), dtype='float32')
     data_val = convert_to_tensor(np.array(data_val), dtype='float32')
     data_test = convert_to_tensor(np.array(data_test), dtype='float32')
-    
+
     return data_train, data_val, data_test
 
 def normalize_data(data):
@@ -53,10 +79,10 @@ def normalize_data(data):
     Returns:
         data (tf.Tensor): Normalized data.
     """
-    
+
     data = (data - 127.5) / 127.5
     return data
-    
+
 def split_images(data):
     """Split images from data (with shapes (256, 512, 3)) into two images (with shapes (256, 256, 3)).
 
@@ -66,11 +92,11 @@ def split_images(data):
     Returns:
         X, Y (tf.tensor): Data split between left images (X) and right images (Y).
     """
-    
+
     X = data[:, :, 256:]
     Y = data[:, :, :256]
     return X, Y
-    
+
 def create_dataset(X, batch_size=16) :
     """Create a tf.Dataset from a tf.tensor, given a batch size.
 
@@ -81,7 +107,7 @@ def create_dataset(X, batch_size=16) :
     Returns:
         X_ds (tf.Dataset)
     """
-    
+
     return Dataset.from_tensor_slices(X).batch(batch_size)
 
 def get_facades_datasets(host='drive', batch_size=16):
@@ -94,20 +120,21 @@ def get_facades_datasets(host='drive', batch_size=16):
     Returns:
         paint_ds_train, ..., real_ds_test : The 6 tf.Dataset needed - (X, Y) from (train, val, test)
     """
-    
+
     data_train, data_val, data_test = load_data(host, dataset='facades')
+    print('ok')
     data_train, data_val, data_test = normalize_data(data_train), normalize_data(data_val), normalize_data(data_test)
     paint_train, real_train = split_images(data_train)
     paint_val, real_val = split_images(data_val)
     paint_test, real_test = split_images(data_test)
-    
+
     paint_ds_train, real_ds_train = create_dataset(paint_train, batch_size), create_dataset(real_train, batch_size)
     paint_ds_val, real_ds_val = create_dataset(paint_val, batch_size), create_dataset(real_val, batch_size)
     paint_ds_test, real_ds_test = create_dataset(paint_test, batch_size), create_dataset(real_test, batch_size)
-    
+
     return paint_ds_train, paint_ds_val, paint_ds_test, real_ds_train, real_ds_val, real_ds_test
-    
+
 if __name__ == "__main__":
     paint_ds_train, paint_ds_val, paint_ds_test, real_ds_train, real_ds_val, real_ds_test = get_facades_datasets(host='local')
-    print(type(paint_ds_test))
-    print(real_ds_val)
+    #print(type(paint_ds_test))
+    #print(real_ds_val)
